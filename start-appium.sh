e81ec7ad711433ab8dee37f3ca34ec7065bbe50f
Shoaib Mansoor <shoaibmansoor@live.in>
2023-09-10 03:15:53 +0530

    update timeout

--- start-appium.sh
+++ start-appium.sh
@@ -3,7 +3,7 @@
 # genymotion start instance
 gmsaas config set android-sdk-path $ANDROID_HOME
 gmsaas auth login $GM_USERNAME $GM_PASSWORD
-gmsaas instances start 95016679-8f8d-4890-b026-e4ad889aadf1 hr-dashboard-emulator --max-run-duration 10
+gmsaas instances start 95016679-8f8d-4890-b026-e4ad889aadf1 hr-dashboard-emulator --max-run-duration 30
 gmsaas instances list > instances.txt
 cat instances.txt
 sleep 20
